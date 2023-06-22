4b6082aa18a18964b98758bf80204a147129a79b
Mussorable <redduck5601@gmail.com>
2023-06-22 21:21:50 +0200

    add html files, routes, helpers folder with rootDir function

--- first-app.js
+++ first-app.js
@@ -1,25 +1,20 @@
 const express = require("express");
 
+const path = require("path");
+
+const adminRoutes = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+
 const bodyParses = require("body-parser");
 
 const app = express();
 
 app.use(bodyParses.urlencoded({ extended: false }));
+app.use("/admin", adminRoutes);
+app.use(shopRouter);
 
-app.use("/users", (req, res) => {
-  console.log('Inside "/users" endopint');
-  res.send(
-    '<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
-  );
-});
-
-app.use("/add-product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>inside /</h1>");
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "e404.html"));
 });
 
 app.listen(5173);
---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const path = require("path");
+
+const rootDir = require("../helpers/path");
+
+const router = express.Router();
+
+router.get("/add-product", (req, res) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+});
+
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
