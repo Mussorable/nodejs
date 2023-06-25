fb6fe954e85cefc24c872b9ccfdfcf98cc1a4e08
Mussorable <redduck5601@gmail.com>
2023-06-25 09:23:42 +0200

    added details page with dynamic parameter

--- models/product.js
+++ models/product.js
@@ -1,25 +1,32 @@
 const fs = require("fs");
 const path = require("path");
 
-const p = path.join(__dirname, "..", "data", "products.json");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
 
 const getProductsFromFile = (cb) => {
-  fs.readFile(p, (error, data) => {
-    fs.readFile(p, (error, data) => {
-      if (error) {
-        return cb([]);
-      }
-      cb(JSON.parse(data));
-    });
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -28,7 +35,14 @@ module.exports = class Product {
     });
   }
 
-  static getAll(cb) {
+  static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findByIfd(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((item) => item.id === id);
+      cb(product);
+    });
   }
 };
